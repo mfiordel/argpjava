3505e755449c46f8eb904e0e521b2478c2b0297d
Micael Fiordelmondo <micael.fiordel@gmail.com>
2022-06-29 12:17:29 -0300

    java first commit

---
+++ src/main/java/com/portfolio/mfiordel/MfiordelApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.mfiordel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfiordelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfiordelApplication.class, args);
+	}
+
+}
